42558cf96552f4cf332824e059cb416e861f26d7
Navya Aneja <navyaaneja25@gmail.com>
2024-10-26 17:22:21 +0530

    Create questwo.c

---
+++ NavyaAneja.txt/questwo.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <string.h> 
+
+int main() 
+{ 
+    char str[] = "I am Sid"; 
+    int i, j = 0;  
+    for (i = 0; i < strlen(str); i++) { 
+        if (i == 0 && str[i] != ' ') { 
+            printf("%c ", str[i]); 
+        }  
+        else if (i > 0 && str[i - 1] == ' ') { 
+            printf("%c ", str[i]); 
+        } 
+    } 
+    return 0; 
+}
